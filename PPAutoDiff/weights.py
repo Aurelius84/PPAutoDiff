ea8e05c83b08f8fcfcc37b8f9ddeec5cd580d453
feifei-111 <2364819892@qq.com>
2022-12-27 19:37:06 +0800

    Add check_weight_grad (#5)
    * bugfix
    
    * add check_weight_grad

--- PPAutoDiff/weights.py
+++ PPAutoDiff/weights.py
@@ -39,14 +39,58 @@ def _assign_weight(paddle_sublayer, torch_submodule, param_name, paddle_param, t
             _do_assign(torch_param, numpy.transpose(np_value), "torch")
 
 
-def check_weight_grad(layer, module):
-    def _check_weight_grad(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param): 
-        # TODO(zhanfei): add weigth compare function.
-        pass
-    map_for_each_weight(_check_weight_grad, layer, module)
-
 def assign_weight(layer, module):
     map_for_each_weight(_assign_weight, layer, module)
+
+
+def check_weight_grad(layer, module, options):
+
+    _weight_check = True
+    _grad_check = True
+
+    def _check_weight_grad(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param):
+        nonlocal _weight_check, _grad_check
+        atol = options.get("atol", 1e-7)
+        transpose = False
+        yaml_path = osp.join(osp.dirname(__file__), "configs", "assign_weight.yaml")
+        assign_config = yaml.safe_load(open(yaml_path, "r"))
+        config = assign_config.get(paddle_sublayer.__class__.__name__, None)
+        if config is not None: 
+            assert torch_submodule.__class__.__name__ == config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
+            if param_name in config['param'] and config['param'][param_name] == "transpose": 
+                transpose = True
+
+        p_param = paddle_param.numpy()
+        t_param = torch_param.detach().numpy()
+        p_grad = paddle_param.grad.numpy()
+        t_grad = torch_param.grad.detach().numpy()
+        if transpose:
+            t_param = numpy.transpose(t_param)
+            t_grad = numpy.transpose(t_grad)
+
+        assert list(p_param.shape) == list(t_param.shape), ("After training, the shape is different for param `{}`.\n"
+                    "paddle: shape `{}` at `{}`.\n"
+                    "torch: shape `{}` at `{}`.\n"
+                    ).format(param_name, p_param.shape, paddle_sublayer, t_param.shape, torch_submodule)
+
+        if not numpy.allclose(p_param, t_param, atol=atol):
+            _weight_check = False
+            print("After training, weight value is different for param `{}`.\n"
+                    "paddle: at `{}`.\n"
+                    "torch: at `{}`.\n"
+                    .format(param_name, paddle_sublayer, torch_submodule))
+
+        
+        if not numpy.allclose(p_grad, t_grad, atol=atol):
+            _grad_check = False 
+            print("After training, grad value is different for param `{}`.\n"
+                    "paddle: at `{}`.\n"
+                    "torch: at `{}`.\n"
+                    .format(param_name, paddle_sublayer, torch_submodule))
+    
+    map_for_each_weight(_check_weight_grad, layer, module)
+    return _weight_check, _grad_check
+
 
 def remove_inplace(layer, module):
     def _remove_inplace(layer, module):
