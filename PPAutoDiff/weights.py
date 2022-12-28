d2cb86ff73e8731b3ae932778f39549d7de1660c
feifei-111 <2364819892@qq.com>
2022-12-28 08:13:48 +0000

    add each_weight_fn_factory

--- PPAutoDiff/weights.py
+++ PPAutoDiff/weights.py
@@ -2,58 +2,63 @@ import paddle
 import torch
 import numpy
 import yaml
+import contextlib
 import os.path as osp
 from .utils import map_for_each_weight, map_for_each_sublayer
 
-def _assign_weight(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param):
+_weight_check = True
+_grad_check = True
 
-    def _do_assign(param, np_value, type="paddle"): 
-        assert list(param.shape) == list(np_value.shape), ("Shape is not the same. {} vs {} \n"
-                    "Hint: \n"
-                    "      1. check whether your paddle model definition and torch model definition are corresponding.\n"
-                    "      2. check the weight shape of paddle:`{}` and torch:`{}` is the same.\n"
-                    ).format(param.shape, np_value.shape, paddle_sublayer, torch_submodule)
-        if type == "paddle":
-            paddle.assign(paddle.to_tensor(np_value), param)
-        elif type == "torch":
-            param.data = torch.as_tensor(np_value).type(param.dtype)
-        else: 
-            raise RuntimeError("Invalid Arguments, type must be one of ['paddle', 'torch'].")
-
-    shape = paddle_param.shape
-    np_value = paddle.randn(shape).numpy()
-    yaml_path = osp.join(osp.dirname(__file__), "configs", "assign_weight.yaml")
-    assign_config = yaml.safe_load(open(yaml_path, "r"))
-    config = assign_config.get(paddle_sublayer.__class__.__name__, None)
-    if config is not None: 
-        assert torch_submodule.__class__.__name__ == config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
-    if config is None or param_name not in config['param']: 
-        _do_assign(paddle_param, np_value, "paddle")
-        _do_assign(torch_param, np_value, "torch")
-    else: 
-        """
-        TODO: has more options? make it more elegant, remove the if-elif by MethodClass.
-        """
-        if config['param'][param_name] == "transpose": 
-            _do_assign(paddle_param, np_value, "paddle")
-            _do_assign(torch_param, numpy.transpose(np_value), "torch")
-
-
-def assign_weight(layer, module):
-    map_for_each_weight(_assign_weight, layer, module)
-
-
-def check_weight_grad(layer, module, options):
-
+@contextlib.contextmanager
+def weight_grad_check_guard():
+    global _weight_check, _grad_check
+    old_weight_check = _weight_check
+    old_grad_check = _grad_check
     _weight_check = True
     _grad_check = True
+    yield
+    _weight_check = old_weight_check
+    _grad_check = old_grad_check
+
+def each_weight_fn_factory(fn_name, options=None):
+    yaml_path = osp.join(osp.dirname(__file__), "configs", "assign_weight.yaml")
+    assign_config = yaml.safe_load(open(yaml_path, "r"))
+
+    def _assign_weight(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param):
+
+        def _do_assign(param, np_value, type="paddle"): 
+            assert list(param.shape) == list(np_value.shape), ("Shape is not the same. {} vs {} \n"
+                        "Hint: \n"
+                        "      1. check whether your paddle model definition and torch model definition are corresponding.\n"
+                        "      2. check the weight shape of paddle:`{}` and torch:`{}` is the same.\n"
+                        ).format(param.shape, np_value.shape, paddle_sublayer, torch_submodule)
+            if type == "paddle":
+                paddle.assign(paddle.to_tensor(np_value), param)
+            elif type == "torch":
+                param.data = torch.as_tensor(np_value).type(param.dtype)
+            else: 
+                raise RuntimeError("Invalid Arguments, type must be one of ['paddle', 'torch'].")
+
+        shape = paddle_param.shape
+        np_value = paddle.randn(shape).numpy()
+        config = assign_config.get(paddle_sublayer.__class__.__name__, None)
+        if config is not None: 
+            assert torch_submodule.__class__.__name__ == config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
+        if config is None or param_name not in config['param']: 
+            _do_assign(paddle_param, np_value, "paddle")
+            _do_assign(torch_param, np_value, "torch")
+        else: 
+            """
+            TODO: has more options? make it more elegant, remove the if-elif by MethodClass.
+            """
+            if config['param'][param_name] == "transpose": 
+                _do_assign(paddle_param, np_value, "paddle")
+                _do_assign(torch_param, numpy.transpose(np_value), "torch")
 
     def _check_weight_grad(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param):
-        nonlocal _weight_check, _grad_check
+        global _weight_check, _grad_check
         atol = options.get("atol", 1e-7)
         transpose = False
-        yaml_path = osp.join(osp.dirname(__file__), "configs", "assign_weight.yaml")
-        assign_config = yaml.safe_load(open(yaml_path, "r"))
         config = assign_config.get(paddle_sublayer.__class__.__name__, None)
         if config is not None: 
             assert torch_submodule.__class__.__name__ == config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
@@ -88,8 +93,26 @@ def check_weight_grad(layer, module, options):
                     "torch: at `{}`.\n"
                     .format(param_name, paddle_sublayer, torch_submodule))
     
-    map_for_each_weight(_check_weight_grad, layer, module)
-    return _weight_check, _grad_check
+    fn_family = {
+        'assign_weight' : _assign_weight,
+        'check_weight_grad' : _check_weight_grad,
+    }
+
+    if fn_name not in fn_family.keys():
+        raise RuntimeError("Invalid fn type, not such fn called `{}`".format(fn_name))
+
+    return fn_family[fn_name]
+
+def assign_weight(layer, module):
+    map_for_each_weight(each_weight_fn_factory('assign_weight'), layer, module)
+
+
+def check_weight_grad(layer, module, options):
+    with weight_grad_check_guard():
+        map_for_each_weight(each_weight_fn_factory('check_weight_grad', options), layer, module)
+        w_check = _weight_check
+        g_check = _grad_check
+    return w_check, g_check
 
 
 def remove_inplace(layer, module):
