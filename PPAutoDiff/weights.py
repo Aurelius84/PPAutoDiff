d4dde33aa0829e1068793e00084a55343d21ed84
feifei-111 <2364819892@qq.com>
2022-12-28 10:57:22 +0000

    update process_each_weight

--- PPAutoDiff/weights.py
+++ PPAutoDiff/weights.py
@@ -2,119 +2,110 @@ import paddle
 import torch
 import numpy
 import yaml
-import contextlib
 import os.path as osp
+from itertools import zip_longest
 from .utils import map_for_each_weight, map_for_each_sublayer
 
-_weight_check = True
-_grad_check = True
 
+def process_each_weight(process_name, layer, module, options=None):
+    if options is None:
+        options = {"atol": 1e-7}
+    yaml_path = osp.join(osp.dirname(__file__), "configs", "assign_weight.yaml")
+    assign_yaml = yaml.safe_load(open(yaml_path, "r"))
 
-@contextlib.contextmanager
-def weight_grad_check_guard():
-    global _weight_check, _grad_check
-    old_weight_check = _weight_check
-    old_grad_check = _grad_check
+    yamls = {
+        'assign_yaml' : assign_yaml,
+    }
+
+    def _process_runner(process, paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, yamls):
+        assign_config = yamls['assign_yaml'].get(paddle_sublayer.__class__.__name__, None)
+        atol = options.get('atol', 1e-7)
+
+        if assign_config is not None:
+            assert torch_submodule.__class__.__name__ == assign_config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
+        if assign_config is None or param_name not in assign_config['param']:
+            process(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, 
+                settings={'transpose':False, 'atol': atol}
+            )
+        else:
+            if assign_config['param'][param_name] == "transpose": 
+                process(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, 
+                    settings={'transpose':True, 'atol': atol}
+                )
+
+    def _shape_check(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, settings):
+        p_shape = list(paddle_param.shape)
+        t_shape = list(torch_param.shape)
+        if settings['transpose']:
+            t_shape.reverse()
+        assert p_shape == t_shape, ("Shape of param `{}` is not the same. {} vs {}\n"
+                    "Hint: \n"
+                    "      1. check whether your paddle model definition and torch model definition are corresponding.\n"
+                    "      2. check the weight shape of paddle:`{}` and torch:`{}` is the same.\n"
+                    ).format(param_name, p_shape, t_shape, paddle_sublayer, torch_submodule)
+
+    def _assign_weight(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, settings):
+        _shape_check(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, settings)
+        np_value = paddle.randn(paddle_param.shape).numpy()
+        paddle.assign(paddle.to_tensor(np_value), paddle_param)
+        if settings['transpose']:
+            torch_param.data = torch.as_tensor(numpy.transpose(np_value)).type(torch_param.dtype)
+        else:
+            torch_param.data = torch.as_tensor(np_value).type(torch_param.dtype)
+
     _weight_check = True
     _grad_check = True
-    yield
-    _weight_check = old_weight_check
-    _grad_check = old_grad_check
 
-
-def each_weight_fn_factory(fn_name, options=None):
-    yaml_path = osp.join(osp.dirname(__file__), "configs", "assign_weight.yaml")
-    assign_config = yaml.safe_load(open(yaml_path, "r"))
-
-    def _assign_weight(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param):
-
-        def _do_assign(param, np_value, type="paddle"): 
-            assert list(param.shape) == list(np_value.shape), ("Shape is not the same. {} vs {} \n"
-                        "Hint: \n"
-                        "      1. check whether your paddle model definition and torch model definition are corresponding.\n"
-                        "      2. check the weight shape of paddle:`{}` and torch:`{}` is the same.\n"
-                        ).format(param.shape, np_value.shape, paddle_sublayer, torch_submodule)
-            if type == "paddle":
-                paddle.assign(paddle.to_tensor(np_value), param)
-            elif type == "torch":
-                param.data = torch.as_tensor(np_value).type(param.dtype)
-            else: 
-                raise RuntimeError("Invalid Arguments, type must be one of ['paddle', 'torch'].")
-
-        shape = paddle_param.shape
-        np_value = paddle.randn(shape).numpy()
-        config = assign_config.get(paddle_sublayer.__class__.__name__, None)
-        if config is not None: 
-            assert torch_submodule.__class__.__name__ == config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
-        if config is None or param_name not in config['param']: 
-            _do_assign(paddle_param, np_value, "paddle")
-            _do_assign(torch_param, np_value, "torch")
-        else: 
-            """
-            TODO: has more options? make it more elegant, remove the if-elif by MethodClass.
-            """
-            if config['param'][param_name] == "transpose": 
-                _do_assign(paddle_param, np_value, "paddle")
-                _do_assign(torch_param, numpy.transpose(np_value), "torch")
-
-    def _check_weight_grad(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param):
-        global _weight_check, _grad_check
-        atol = options.get("atol", 1e-7)
-        transpose = False
-        config = assign_config.get(paddle_sublayer.__class__.__name__, None)
-        if config is not None: 
-            assert torch_submodule.__class__.__name__ == config['torch'], "Not correspond, check your __init__ to make sure every sublayer is corresponded."
-            if param_name in config['param'] and config['param'][param_name] == "transpose": 
-                transpose = True
-
+    def _check_weight_grad(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, settings):
+        nonlocal _weight_check, _grad_check
+        _shape_check(paddle_sublayer, torch_submodule, param_name, paddle_param, torch_param, settings)
         p_param = paddle_param.numpy()
         t_param = torch_param.detach().numpy()
         p_grad = paddle_param.grad.numpy()
         t_grad = torch_param.grad.detach().numpy()
-        if transpose:
+        if settings['transpose']:
             t_param = numpy.transpose(t_param)
             t_grad = numpy.transpose(t_grad)
 
-        assert list(p_param.shape) == list(t_param.shape), ("After training, the shape is different for param `{}`.\n"
-                    "paddle: shape `{}` at `{}`.\n"
-                    "torch: shape `{}` at `{}`.\n"
-                    ).format(param_name, p_param.shape, paddle_sublayer, t_param.shape, torch_submodule)
-
-        if not numpy.allclose(p_param, t_param, atol=atol):
+        if not numpy.allclose(p_param, t_param, atol=settings['atol']):
             _weight_check = False
             print("After training, weight value is different for param `{}`.\n"
                     "paddle: at `{}`.\n"
                     "torch: at `{}`.\n"
                     .format(param_name, paddle_sublayer, torch_submodule))
 
-        
-        if not numpy.allclose(p_grad, t_grad, atol=atol):
+
+        if not numpy.allclose(p_grad, t_grad, atol=settings['atol']):
             _grad_check = False 
             print("After training, grad value is different for param `{}`.\n"
                     "paddle: at `{}`.\n"
                     "torch: at `{}`.\n"
                     .format(param_name, paddle_sublayer, torch_submodule))
-    
-    fn_family = {
+
+    process_family = {
         'assign_weight' : _assign_weight,
         'check_weight_grad' : _check_weight_grad,
     }
 
-    if fn_name not in fn_family.keys():
-        raise RuntimeError("Invalid fn type, not such fn called `{}`".format(fn_name))
+    if process_name not in process_family.keys():
+        raise RuntimeError("Invalid fn type, not such fn called `{}`".format(process_name))
 
-    return fn_family[fn_name]
+    for paddle_sublayer, torch_submodule in zip_longest(layer.sublayers(True), module.modules(), fillvalue=None): 
+        if paddle_sublayer is None or torch_submodule is None: 
+            raise RuntimeError("Torch and Paddle return difference number of sublayers. Check your model.")
+        for (name, paddle_param), torch_param in zip(paddle_sublayer.named_parameters("",False), torch_submodule.parameters(False)): 
+            _process_runner(process_family[process_name], paddle_sublayer, torch_submodule, name, paddle_param, torch_param, yamls)
+
+    if process_name == 'check_weight_grad':
+        return _weight_check, _grad_check
 
 
 def assign_weight(layer, module):
-    map_for_each_weight(each_weight_fn_factory('assign_weight'), layer, module)
+    process_each_weight('assign_weight', layer, module)
 
 
 def check_weight_grad(layer, module, options):
-    with weight_grad_check_guard():
-        map_for_each_weight(each_weight_fn_factory('check_weight_grad', options), layer, module)
-        w_check = _weight_check
-        g_check = _grad_check
+    w_check, g_check = process_each_weight('check_weight_grad', layer, module, options)
     return w_check, g_check
 
 
